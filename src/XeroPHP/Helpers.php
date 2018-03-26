791bbc04c8d773bf6988828beefcb01a3ebd0ed3
Alex Babkov <alex.babkov@rexsoftware.com.au>
2018-03-26 16:51:07 +1000

    Added exception for LeaveEarningsLine and EarningsLine handling

--- src/XeroPHP/Helpers.php
+++ src/XeroPHP/Helpers.php
@@ -80,8 +80,10 @@ class Helpers
 
             //don't make it assoc, as the keys will all be the same
             if ($child_name === $singular_node_name ||
-                //Handle strange XML
-                ($singular_node_name === 'Tracking' && $child_name === TrackingCategory::getRootNodeName())) {
+                //Handle exceptions where parent node and child node have different names
+                ($singular_node_name === 'Tracking' && $child_name === TrackingCategory::getRootNodeName()) ||
+                ($singular_node_name === 'LeaveEarningsLine' && $child_name === 'EarningsLine')
+            ) {
                 $output[] = $node;
             } else {
                 $output[$child_name] = $node;
